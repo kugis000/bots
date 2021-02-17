bb5e165122170d0b80073b177b0ea6ba0f0583ba
kugis000 <58030143+kugis000@users.noreply.github.com>
2021-02-17 11:48:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,7 @@
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const bot = new Client();
 
- 
-
-bot.on('ready', () => {
- 
- console.log('This bot is online!');
- bot.user.setActivity('YouTube', { type: 'WATCHING' }).catch(console.error);
-});
 
 client.on('ready', () => {
 
